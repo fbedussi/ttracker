5c2e46ac05785c2faf6588dc0f252af3aa67154d
Francesco Bedussi <francesco.bedussi@ynap.com>
2017-08-29 18:47:38 +0200

    progress

--- src/App.js
+++ src/App.js
@@ -42,7 +42,8 @@ class App extends Component {
     super(props);
 
     this.state = {
-      menuOpen: false
+      menuOpen: false,
+      clients: ttracker
     }
   }
   render() {
@@ -77,25 +78,144 @@ class App extends Component {
                   <p>1/1/2017</p>
                   <Subheader>Next invoice subtotal</Subheader>
                   <p>€ 1,000</p>
-                    <Subheader>Projects</Subheader>
+                    <Subheader>Projects 
+                      <FlatButton
+                        icon={<ContentAdd />}
+                      />
+                    </Subheader>
                     <div style={styles.wrapper}>
                     <Chip
                       style={styles.chip}
+                      onRequestDelete={() => {}}
+                      
                     >
                     project1
                   </Chip>
                   <Chip
                       style={styles.chip}
+                      onRequestDelete={() => {}}
+                      
                     >
                     project2
                   </Chip>
                   </div>
+                
                   </CardText>
                 </Card>
 
                 <Card>
                   <CardHeader
                     title="Client Name 2"
+                    actAsExpander={true}
+                    showExpandableButton={true}
+                  />
+                  <CardActions>
+                    <FlatButton label="Edit" icon={<EditIcon />} />
+                    <FlatButton label="Delete" icon={<DeleteIcon />} />
+                  </CardActions>
+                  <CardText expandable={true}>
+                  <Subheader>Last billed date</Subheader>
+                  <p>1/1/2017</p>
+                  <Subheader>Next invoice subtotal</Subheader>
+                  <p>€ 1,000</p>
+                    <Subheader>Projects</Subheader>
+                    <div style={styles.wrapper}>
+                    <Chip
+                      style={styles.chip}
+                      onRequestDelete={() => {}}
+                    >
+                    project1
+                  </Chip>
+                  <Chip
+                      style={styles.chip}
+                      onRequestDelete={() => {}}
+                    >
+                    project2
+                  </Chip>
+                  </div>
+                  </CardText>
+                </Card>
+              </Tab>
+              <Tab
+                label="Projects"
+              >
+              <Card>
+                  <CardHeader
+                    title="Project Name 1"
+                    subtitle="Client Name"
+                    actAsExpander={true}
+                    showExpandableButton={true}
+                  />
+                  <CardActions>
+                    <FlatButton label="Edit" icon={<EditIcon />} />
+                    <FlatButton label="Delete" icon={<DeleteIcon />} />
+                  </CardActions>
+                  <CardText expandable={true}>
+                  <Subheader>Total time</Subheader>
+                  <p>22 hrs</p>
+                  <Subheader>Total cost</Subheader>
+                  <p>€ 1,000</p>
+                  <Subheader>Last billed date</Subheader>
+                  <p>1/1/2017</p>
+                  <Subheader>Next invoice subtotal</Subheader>
+                  <p>€ 1,000</p>
+                  <Subheader>Tasks</Subheader>
+                    <div style={styles.wrapper}>
+                    <Chip
+                      style={styles.chip}
+                      onRequestDelete={() => {}}
+                      
+                    >
+                    task1
+                  </Chip>
+                  <Chip
+                      style={styles.chip}
+                      onRequestDelete={() => {}}
+                      
+                    >
+                    task2
+                  </Chip>
+                  </div>
+                  <Subheader>Time entries</Subheader>
+                  <Table>
+    <TableHeader
+    adjustForCheckbox={false}
+    >
+      <TableRow>
+        <TableHeaderColumn>Start time</TableHeaderColumn>
+        <TableHeaderColumn>End time</TableHeaderColumn>
+        <TableHeaderColumn>Duration</TableHeaderColumn>
+        <TableHeaderColumn>Cost</TableHeaderColumn>
+        <TableHeaderColumn>Edit</TableHeaderColumn>
+        <TableHeaderColumn>Delete</TableHeaderColumn>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      <TableRow>
+        <TableRowColumn>1/1/2017 16:00</TableRowColumn>
+        <TableRowColumn>1/1/2017 17:00</TableRowColumn>
+        <TableRowColumn>1 hr</TableRowColumn>
+        <TableRowColumn>€ 10</TableRowColumn>
+        <TableRowColumn><EditIcon/></TableRowColumn>
+        <TableRowColumn><DeleteIcon/></TableRowColumn>
+      </TableRow>
+      <TableRow>
+      <TableRowColumn>1/2/2017 16:00</TableRowColumn>
+      <TableRowColumn>1/2/2017 17:00</TableRowColumn>
+      <TableRowColumn>1 hr</TableRowColumn>
+      <TableRowColumn>€ 10</TableRowColumn>
+      <TableRowColumn><EditIcon/></TableRowColumn>
+      <TableRowColumn><DeleteIcon/></TableRowColumn>
+      </TableRow>
+    </TableBody>
+  </Table>
+                  </CardText>
+                </Card>
+
+                <Card>
+                  <CardHeader
+                    title="Project Name 2"
+                    subtitle="Client Name"
                     actAsExpander={true}
                     showExpandableButton={true}
                   />
@@ -123,11 +243,6 @@ class App extends Component {
                   </div>
                   </CardText>
                 </Card>
-              </Tab>
-              <Tab
-                label="Projects"
-              >
-
 
               </Tab>
             </Tabs>
