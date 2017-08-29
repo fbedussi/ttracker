0b787b7bcaf560f2710eeba48eb4bd582eece4fc
Francesco Bedussi <francesco.bedussi@ynap.com>
2017-08-29 18:49:37 +0200

    app back init

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,43 @@
-import idMaker from './helpers/idMaker';
+import db from '../db/dbFacade';
 
-const clients = [];
-const projects = [];
+const App = {
+    clients: [],
+    projects: [],
+    defaultHourlyRate: 0,
+    load: function() {
+        db
+            .readAll('client')
+            .then(clients => this.clients = client)
+        ;
+        db
+            .readAll('activity')
+            .then(activities => this.activities = activities)
+        ;
+        return this;
+    },
+    getAllProjects: function() {
+        return this.projects;
+    },
+    getAllClients: function() {
+        return this.clients;
+    },
+    getProject: function(id) {
+        return this.projects.filter((project) => project.id = id);
+    },
+    getClient: function(id) {   
+        return this.clients.filter((client) => client.id = id);        
+    },
+    getTotalToBill: function() {
+        return this.clients.reduce((total, client) => total + client.get, 0);
 
-const clientsIdMaker = idMaker();
-const projectsIdMaker = idMaker();
+    },
+    createNewClient: function() {
+
+    }
+}
+
+const createApp = (conf) => {
+    return Object.create(App).load();
+}
+
+export default createApp;
