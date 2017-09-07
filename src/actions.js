d075a1cd05f21b32b3c91c6d7c4c23c82a4f1941
Francesco Bedussi <francesco.bedussi@ynap.com>
2017-09-07 08:43:48 +0200

    merge2

--- src/actions.js
+++ src/actions.js
@@ -1,4 +1,5 @@
-import loadBackend from './backend/app'
+import loadBackend from './backend/app';
+import {getOnlyOwnProperies} from './helpers/helpers';
 
 var backend;
 
@@ -10,8 +11,13 @@ export function load() {
                 
                 dispatch({
                     type: 'LOAD_APP', 
-                    clients: backend.clients,
-                    activities: backend.activities
+                    clients: backend.clients
+                        .map((client) => {
+                            var clientData = getOnlyOwnProperies(client)
+                            clientData.activities = clientData.activities.map((activity) => activity.id)
+                            return clientData;
+                    }),
+                    activities: backend.activities.map((activity) => getOnlyOwnProperies(activity))
                 });
             })
         ;
@@ -23,12 +29,12 @@ export function createNewClient() {
 
     return {
         type: 'ADD_CLIENT',
-        client
+        client: getOnlyOwnProperies(client)
     }
 }
 
 export function deleteClient(client) {
-    client.delete();
+    backend.getClient(client.id).delete();
 
     return {
         type: 'REMOVE_CLIENT',
@@ -46,11 +52,21 @@ export function addNewActivity(activity) {
 export function createNewActivity() {
     const activity = backend.createNewActivity();
 
-    return addNewActivity(activity);
+    return addNewActivity(getOnlyOwnProperies(activity));
+}
+
+export function addNewActivityToClient(clientId) {
+    const activity = backend.addNewActivityToClient(clientId);
+
+    return {
+        type: 'ADD_NEW_ACTIVITY_TO_CLIENT',
+        clientId: clientId,
+        activity: activity
+    }
 }
 
 export function deleteActivity(activity) {
-    activity.delete();
+    backend.getActivity(activity.id).delete();
 
     return {
         type: 'REMOVE_ACTIVITY',
@@ -73,7 +89,7 @@ export function updateClient(client) {
 }
 
 export function startActivity(activity) {
-    activity.start();
+    backend.getActivity(activity.id).start();
 
     return {
         type: 'START_ACTIVITY',
@@ -82,7 +98,7 @@ export function startActivity(activity) {
 }
 
 export function stopActivity(activity) {
-    activity.stop();
+    backend.getActivity(activity.id).stop();
 
     return {
         type: 'STOP_ACTIVITY',
@@ -91,11 +107,59 @@ export function stopActivity(activity) {
 }
 
 export function deleteTimeEntry(timeEntry, activity) {
-    timeEntry.delete();
+    backend.getActivity(activity.id).removeTimeEntry(timeEntry.id);
 
     return {
         type: 'REMOVE_TIMEENTRY',
         timeEntryId: timeEntry.id,
         activityId: activity.id
+    }
+}
+
+export function changeActivityName(activity, newName) {
+    backend.getActivity(activity.id).update({name: newName});
+    
+    const updatedActivity = Object.assign({}, activity, {name: newName});
+
+    return {
+        type: 'UPDATE_ACTIVITY',
+        activity: updatedActivity
+    }
+}
+
+export function changeActivityNameInClient(activity, newName, client) {
+    backend.getActivity(activity.id).update({name: newName});
+    
+    const updatedActivity = Object.assign({}, activity, {name: newName});
+
+    return {
+        type: 'UPDATE_ACTIVITY_IN_CLIENT',
+        activity: updatedActivity,
+        clientId: client.id
+    }
+}
+
+export function disableEditActivity(id) {
+    return {
+        type: 'DISABLE_EDIT_ACTIVITY',
+        activityId: id
+    }
+}
+
+export function changeClientName(client, newName) {
+    backend.getClient(client.id).update({name: newName});
+    
+    const updatedClient = Object.assign({}, client, {name: newName});
+
+    return {
+        type: 'UPDATE_CLIENT',
+        client: updatedClient
+    }
+}
+
+export function disableEditClient(id) {
+    return {
+        type: 'DISABLE_EDIT_CLIENT',
+        clientId: id
     }
 }
--- src/reducers.js
+++ src/reducers.js
@@ -12,28 +12,69 @@ export default function reducer(state = {
 	switch (action.type) {
 		case 'LOAD_APP':
 			return Object.assign({}, state, {
-                clients: action.clients.map((i) => i),
-                activities: action.activities.map((i) => i)
+                clients: action.clients,
+                activities: action.activities
             });
            
         case 'ADD_CLIENT':
             return Object.assign({}, state, {
-                clients: state.clients.concat(action.client)
+                clients: state.clients.concat(Object.assign({}, action.client, {editableName: true}))
             });
 
         case 'REMOVE_CLIENT':
             return Object.assign({}, state, {
                 clients: state.clients.filter((client) => client.id !== action.id)
             });
+        
+        case 'UPDATE_CLIENT':
+            return Object.assign({}, state, {
+                clients: state.clients.map((client) => client.id === action.client.id ? action.client : client)
+            });
+        
+        case 'ADD_NEW_ACTIVITY_TO_CLIENT':
+            return Object.assign({}, state, {
+                activities: state.activities.concat(Object.assign({}, action.activity, {editableName: true})),
+                clients: state.clients.map((client) => client.id === action.clientId ? 
+                    Object.assign({}, client, {
+                        activities: client.activities.concat(action.activity.id)
+                    })
+                    : client)
+            })
+
+        case 'DISABLE_EDIT_CLIENT':
+            return Object.assign({}, state, {
+                clients: state.clients.map((client) => (!action.clientId || client.id === action.clientId) ? Object.assign({}, client, {editableName: false}) : client)
+            });
 
         case 'ADD_ACTIVITY':
             return Object.assign({}, state, {
-                activities: state.activities.concat(action.activity)
+                activities: state.activities.concat(Object.assign({}, action.activity, {editableName: true}))
             });
+
+        case 'DISABLE_EDIT_ACTIVITY':
+            return Object.assign({}, state, {
+                activities: state.activities.map((activity) => (!action.activityId || activity.id === action.activityId) ? Object.assign({}, activity, {editableName: false}) : activity)
+            });
+
 
         case 'REMOVE_ACTIVITY':
             return Object.assign({}, state, {
                 activities: state.activities.filter((activity) => activity.id !== action.id)
+            });
+
+        case 'UPDATE_ACTIVITY':
+            return Object.assign({}, state, {
+                activities: state.activities.map((activity) => activity.id === action.activity.id ? action.activity : activity)
+            });
+
+        case 'UPDATE_ACTIVITY_IN_CLIENT':
+            return Object.assign({}, state, {
+                activities: state.activities.map((activity) => activity.id === action.activity.id ? action.activity : activity),
+                clients: state.clients.map((client) => client.id === action.clientId ? Object.assign({}, client, 
+                    {
+                        activities: client.activities.map((activity) => activity.id === action.activity.id ? action.activity : activity)
+                    }
+                ) : client)                
             });
 
         case 'REMOVE_TIMEENTRY':
@@ -46,19 +87,14 @@ export default function reducer(state = {
                 activeTab: action.activeTab
             });
         
-        case 'UPDATE_CLIENT':
-            return Object.assign({}, state, {
-                clients: state.clients.map((client) => client.id === action.client.id ? action.client : client)
-            });
-        
         case 'START_ACTIVITY':
             return Object.assign({}, state, {
-                activities: state.activities.map((activity) => activity.id === action.id ? Object.assign(activity, {active: true}) : activity)
+                activities: state.activities.map((activity) => activity.id === action.id ? Object.assign({}, activity, {active: true}) : activity)
             });
 
         case 'STOP_ACTIVITY':
             return Object.assign({}, state, {
-                activities: state.activities.map((activity) => activity.id === action.id ? Object.assign(activity, {active: false}) : activity)
+                activities: state.activities.map((activity) => activity.id === action.id ? Object.assign({}, activity, {active: false}) : activity)
             });
 
 		default:
