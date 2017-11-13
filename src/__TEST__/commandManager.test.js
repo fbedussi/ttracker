e561846f974e33f13ef85719c9fc09f1c8c44d7a
Francesco Bedussi <francesco.bedussi@ynap.com>
2017-11-13 16:42:09 +0100

    commandManager

---
+++ src/__TEST__/commandManager.test.js
@@ -0,0 +1,131 @@
+import createCommandManager from '../backend/commandManager';
+
+const FakeApp = {
+    entities: [{
+        id: 0,
+        name: 'old entity',
+        subEntity: 0,
+    }],
+    subEntities: [{
+        id: 0,
+        name: 'subEntity',
+    }],
+    getEntity: function(id) {
+        return this.entities.filter((entity) => entity.id === id)[0];
+    },
+    getSubEntity: function(id) {
+        return this.subEntities.filter((entity) => entity.id === id)[0];
+    },
+    create: jest.fn(function(entity) {
+        if (entity) {
+            if (entity.subEntity) {
+                this.create(subentity)
+            }
+        } else {
+            return {
+                id: 1,
+                name: 'brand new entity',
+            }
+        }
+    }),
+    delete: jest.fn(function(entity) {
+        if (entity.hasOwnProperty('subEntity')) {
+            this.delete(entity.subEntity)
+        }
+    }),
+    update: jest.fn(),
+}
+
+var newFakeApp;
+var commandManager;
+
+beforeEach(() => {
+    newFakeApp = Object.assign({}, FakeApp);    
+    commandManager = createCommandManager(newFakeApp);
+});
+
+test('execute', () => {
+    const entityToCreate = {
+        id: 1,
+        name: 'new entity',
+    };
+    const createEntityAction = commandManager.createAction(['create', entityToCreate], ['delete', entityToCreate])
+    commandManager.execute(createEntityAction)
+
+    expect(newFakeApp.create).toBeCalledWith(entityToCreate);
+});
+
+test('execute without history', () => {
+    const entityToUpdate = Object.assign({},newFakeApp.getEntity(0), {name: 'updated name'});
+
+    const updateEntityAction = commandManager.createAction(['update', entityToUpdate])
+    commandManager.execute(updateEntityAction)
+
+    expect(newFakeApp.update).toBeCalledWith(entityToUpdate);
+    expect(commandManager._history.length).toBe(0);
+});
+
+test('execute without multiple parameters', () => {
+    const updateEntityAction = commandManager.createAction(['update', 1, 2])
+    commandManager.execute(updateEntityAction)
+
+    expect(newFakeApp.update).toBeCalledWith(1, 2);
+});
+
+test('undo', () => {
+    const entityToDelete = newFakeApp.getEntity(0);
+    const deleteEntityAction = commandManager.createAction(['delete', entityToDelete], ['create', entityToDelete])
+    
+    commandManager.execute(deleteEntityAction)
+    commandManager.undo();
+
+    expect(newFakeApp.delete).toBeCalledWith(entityToDelete);
+    expect(newFakeApp.delete).toHaveBeenCalledTimes(2);
+    expect(newFakeApp.create).toBeCalledWith(entityToDelete);
+    expect(newFakeApp.create).toHaveBeenCalledTimes(2);
+});
+
+test('undo very last action', () => {
+    const entityToDelete = newFakeApp.getEntity(0);
+    const deleteEntityAction = commandManager.createAction(['delete', entityToDelete], ['create', entityToDelete]);
+    
+    commandManager.execute(deleteEntityAction)
+    commandManager.undo();
+    const exitCode = commandManager.undo();
+    
+    expect(exitCode).toBe(null);
+});
+
+test('redo', () => {
+    const newFakeAppOriginal = Object.assign({}, newFakeApp);
+    const entityToDelete = newFakeApp.getEntity(0);
+    const deleteEntityAction = commandManager.createAction(['delete', entityToDelete], ['create', entityToDelete]);
+    
+    commandManager.execute(deleteEntityAction)
+    commandManager.undo();
+    commandManager.redo();
+
+    expect(newFakeApp.entities.length).toBe(1);
+    expect(newFakeApp).toEqual(newFakeAppOriginal);
+});
+
+test('redo very last action', () => {
+    const entityToDelete = newFakeApp.getEntity(0);
+    const deleteEntityAction = commandManager.createAction(['delete', entityToDelete], ['create', entityToDelete]);
+    
+    commandManager.execute(deleteEntityAction)
+    commandManager.undo();
+    commandManager.redo();
+    const exitCode = commandManager.redo();
+    
+    expect(exitCode).toBe(null);
+});
+
+test('deferred data', () => {
+    const createEntityAction = commandManager.createAction(['create'], ['delete'], true);
+    
+    commandManager.execute(createEntityAction)
+    commandManager.undo();
+    expect(newFakeApp.entities.length).toBe(1);
+});
+
---
+++ src/backend/commandManager.js
@@ -0,0 +1,92 @@
+const commandManager = {
+    _history: [],
+    _currentActionIndex: -1,
+    _app: null,
+    createAction: function(doAction, undoAction, deferredUndoData = false) {
+        const actionObj = {
+            do: {
+                actionName: doAction[0],
+                data: doAction.slice(1),
+            },
+            deferredUndoData,
+        };
+
+        if (undoAction) {
+            Object.assign(actionObj, {
+                undo: {
+                    actionName: undoAction[0],
+                    data: undoAction.slice(1),
+                },
+            })
+        }
+
+        return actionObj;
+    },
+    _execute: function(appMethodName, appMethodArgs) {
+        return this._app[appMethodName](...[].concat(appMethodArgs)); //concat is to ensure that ... operates on an array even if appMethodArgs is undefined
+    }, 
+    undo: function() {
+        const lastAction = this._history[this._currentActionIndex];
+
+        if (!lastAction) {
+            return null;
+        }
+
+        var returnedData;
+
+        lastAction
+            .reverse()
+            .forEach((subaction) => returnedData = this._execute(subaction.undo.actionName, subaction.undo.data))
+        ;
+        this._currentActionIndex = this._currentActionIndex - 1;
+        
+        return returnedData;
+    },
+    redo: function() {
+        const nextActionIndex = this._currentActionIndex + 1;
+        const nextAction = this._history[nextActionIndex];
+        
+        if (!nextAction) {
+            return null;
+        }
+
+        var returnedData;
+        
+        nextAction.forEach((subaction) => returnedData = this._execute(subaction.do.actionName, subaction.do.data));
+        this._currentActionIndex = nextActionIndex;
+
+        return returnedData;        
+    },
+    execute: function(action, subaction = false) {
+        const returnedData = this._execute(action.do.actionName, action.do.data);
+        
+        if (!action.undo) {
+            return returnedData;
+        }
+        
+        if (action.deferredUndoData) {
+            action.undo.data = returnedData;
+        }
+
+        if (subaction) {
+            const currentAction = this._history[this._history.length - 1].concat(action);
+        } else {
+            this._currentActionIndex = this._currentActionIndex + 1;
+            if (this._currentActionIndex < this._history.length) {
+                this._history = this._history.slice(0,this._currentActionIndex);
+            }
+            this._history = this._history.concat([[action]]);
+        }
+
+        return returnedData;
+    },
+    init: function(app) {
+        this._app = app;
+
+        return this;
+    }
+};
+
+export default function createCommandManager(app) {
+    return Object.create(commandManager).init(app);
+}
